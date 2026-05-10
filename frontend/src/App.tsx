918252629102cdf852e8a9e813633506a9a750c0
chandudevop86 <chandudevop@gmail.com>
2026-05-11 00:41:42 +0530

    Wire frontend routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import AppLayout from "./layouts/AppLayout";
+import Candles from "./pages/Candles";
 import Dashboard from "./pages/Dashboard";
+import Execution from "./pages/Execution";
 import LiveAnalysis from "./pages/LiveAnalysis";
 import Jobs from "./pages/Jobs";
 import Strategies from "./pages/Strategies";
@@ -13,6 +15,8 @@ export default function App() {
       <AppLayout>
         <Routes>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/candles" element={<Candles />} />
+          <Route path="/execution" element={<Execution />} />
           <Route path="/live" element={<LiveAnalysis />} />
           <Route path="/analysis" element={<LiveAnalysis />} />
           <Route path="/jobs" element={<Jobs />} />
