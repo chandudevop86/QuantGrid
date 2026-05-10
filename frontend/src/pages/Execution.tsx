3f9e922296085172ef418d43e4cc064883a6c48e
chandudevop86 <chandudevop@gmail.com>
2026-05-11 00:45:20 +0530

    Implement Execution page

---
+++ frontend/src/pages/Execution.tsx
@@ -0,0 +1,10 @@
+import ExecutionForm from "../components/ExecutionForm";
+
+export default function Execution() {
+  return (
+    <section>
+      <h1>Execution</h1>
+      <ExecutionForm />
+    </section>
+  );
+}
