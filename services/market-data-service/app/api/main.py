5d4217a45cfa3ce213fd128b287cd38814c73b4b
puttaramesh171@gmail.com <puttaramesh171@gmail.com>
2026-07-28 22:15:46 +0530

    Database realted files

--- services/market-data-service/app/api/main.py
+++ services/market-data-service/app/api/main.py
@@ -5,27 +5,19 @@ from app.database.schema import create_tables
 
 app = FastAPI(
     title="QuantGrid Market Data Service",
-    version="1.0"
+    version="1.0.0"
 )
-
 
 
 @app.on_event("startup")
 def startup():
-
     create_tables()
-
 
 
 @app.get("/health")
 def health():
 
     return {
-
-        "service":
-        "market-data-service",
-
-        "status":
-        "running"
-
+        "service": "market-data-service",
+        "status": "running"
     }
