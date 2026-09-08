cd42552b8c76f2d36c90b58496e6e9eb7501da0d
chandudevop86 <chandudevop@gmail.com>
2026-09-08 22:13:21 +0530

    feat(foodtruck): activate new customer ordering UI

--- foodtruck-frontend/src/main.tsx
+++ foodtruck-frontend/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import CustomerPageV2 from './CustomerPageV2.tsx'
+
+const isOwner = window.location.pathname.replace(/\/+$/, '') === '/foodtruck/owner'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {isOwner ? <App /> : <CustomerPageV2 />}
   </StrictMode>,
 )
